7a7c6734e8afc83baef9851446b1e522cacba6b6
Ryan Youngblood <rryb77@gmail.com>
2021-03-04 09:52:58 -0600

    Completed phase 2 and 3

--- Program.cs
+++ Program.cs
@@ -43,7 +43,8 @@ namespace Quest
             int maxAwesomeness = 100;
 
             // Make a new "Adventurer" object using the "Adventurer" class
-            Adventurer theAdventurer = new Adventurer("Jack");
+            Console.Write("What's your name adventurer? ");
+            Adventurer theAdventurer = new Adventurer(Console.ReadLine());
 
             // A list of challenges for the Adventurer to complete
             // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
@@ -75,6 +76,14 @@ namespace Quest
             else
             {
                 Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+            }
+
+            Console.WriteLine("");
+            Console.Write("Would you like to play again? [Y/N]: ");
+            string userInput = Console.ReadLine().ToLower();
+            if (userInput == "y")
+            {
+                Main(args);
             }
         }
     }
