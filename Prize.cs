630f3ee263ca8f6fad8208c1837021208df8c98b
Ryan Youngblood <rryb77@gmail.com>
2021-03-04 10:57:56 -0600

    Started working on the Prize, on step 4 of phase 6

---
+++ Prize.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Quest
+{
+    public class Prize
+    {
+        private string _text;
+
+
+        public Prize(string text)
+        {
+            _text = text;
+        }
+
+        public void ShowPrize(Adventurer adventurer)
+        {
+            if (adventurer.Awesomeness > 0)
+            {
+                for (int i = adventurer.Awesomeness; i > 0; i--)
+                    Console.WriteLine("");
+            }
+        }
+    }
+}
